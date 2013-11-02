b08972c8e515fd95c377950b6da67d7658b4dd3e
alumno <alumno@LAB706-10>
2013-11-02 16:15:09 -0500

    proyecto base

---
+++ src/app/clase2/appclase2.java
@@ -0,0 +1,9 @@
+package app.clase2;
+// psvm +(tab) atajo para public static void main
+// sout +(tab) atajo para imprimir
+public class appclase2 {
+    public static void main(String[] args) {
+        System.out.println("Hola"); 
+        
+    }
+}
